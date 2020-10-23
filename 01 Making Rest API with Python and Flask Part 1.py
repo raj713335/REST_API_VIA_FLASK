1ee527e6dcb367bb3c43772f50f87e6f856cf72c
ARNAB DAS <raj713335@gmail.com>
2020-10-23 20:50:02 +0530

    Initial Commit

--- 01 Making Rest API with Python and Flask Part 1.py
+++ 01 Making Rest API with Python and Flask Part 1.py
@@ -4,3 +4,17 @@ from flask_restful import Resource,Api
 app=Flask(__name__)
 api=Api(app)
 
+class HelloWorld(Resource):
+    def __int__(self):
+        pass
+
+    def get(self):
+        return {
+            "Hello":"World"
+        }
+
+
+api.add_resource(HelloWorld,'/')
+
+if __name__=="__main__":
+    app.run(debug=True)
