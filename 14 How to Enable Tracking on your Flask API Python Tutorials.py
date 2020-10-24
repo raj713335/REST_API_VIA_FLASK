7547ea4839a870381a3d6a5bc15bba6878d24640
ARNAB DAS <raj713335@gmail.com>
2020-10-25 05:25:39 +0530

    Initial Commit

---
+++ 14 How to Enable Tracking on your Flask API Python Tutorials.py
@@ -0,0 +1,23 @@
+from flask import  Flask,g
+app=Flask(__name__)
+
+app.config['TRACK_USAGE_USE_FREEGEOIP']=False
+app.config['TRACK_USAGE_INCLUDE_OR_EXCLUDE_VIEWS']='include'
+
+from flask_track_usage import TrackUsage
+from flask_track_usage.storage.printer import PrintWriter
+from flask_track_usage.storage.output import OutputWriter
+
+t=TrackUsage(app,[
+    PrintWriter(),
+    OutputWriter(transform=lambda s:"OUTPUT: "+str(s))
+])
+
+@t.include
+@app.route('/')
+def index():
+    g.track_var["optional"]="something"
+    return "Hello"
+
+
+app.run(debug=True)
