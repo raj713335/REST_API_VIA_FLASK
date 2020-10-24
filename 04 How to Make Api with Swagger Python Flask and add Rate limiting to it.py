c31a1e968333e343e1a1287c03bc0eaed0bb483e
ARNAB DAS <raj713335@gmail.com>
2020-10-25 01:27:39 +0530

    Initial Commit

--- 04 How to Make Api with Swagger Python Flask and add Rate limiting to it.py
+++ 04 How to Make Api with Swagger Python Flask and add Rate limiting to it.py
@@ -2,4 +2,37 @@ try:
     from flask import Flask,request
     from flask_restful import Resource,Api
 
-    from fl
+    from flask_limiter.util import get_remote_address
+    from flask_limiter import Limiter
+
+    from flasgger import Swagger
+    from flasgger.utils import swag_from
+    from flask_restful_swagger import swagger
+
+except:
+    print("Some modules are missing".format(e))
+
+
+app=Flask(__name__)
+api=Api(app)
+
+limiter=Limiter(app,key_func=get_remote_address)
+limiter.init_app(app)
+
+api = swagger.docs(Api(app),apiVersion='0.1',api_spec_url='/docs')
+
+class MyAPi(Resource):
+
+    decorators=[limiter.limit("2/day")]
+    @swagger.model
+    @swagger.operation(notes='some really good notes')
+    def get(self,zip):
+        return {
+            "Response":200,
+            'Data':zip
+        }
+
+api.add_resource(MyAPi,'/weather/<string:zip>')
+
+if __name__=="__main__":
+    app.run(debug=True)
