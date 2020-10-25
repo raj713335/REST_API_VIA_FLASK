0d59dbfda2aff46d4ffd91b64cb3ef2eb51eeb74
ARNAB DAS <raj713335@gmail.com>
2020-10-26 04:09:23 +0530

    Initial Commit

--- 21 How to use Decorators In Flask REST API  add more functionality & neat code.py
+++ 21 How to use Decorators In Flask REST API  add more functionality & neat code.py
@@ -1,1 +1,50 @@
-print(00)
+from flask import Flask,request
+from flask_restful import Resource,Api
+import time
+import threading
+from functools import wraps
+from datetime import datetime
+import requests
+
+app=Flask(__name__)
+api=Api(app)
+
+def time(function=None):
+
+    @wraps(function)
+    def wrapper(*args,**kwargs):
+        s=datetime.now()
+        _=function(*args,**kwargs)
+        e=datetime.now()
+        print("Execution Time: {}".format(e-s))
+
+        return _
+    return wrapper
+
+
+def monitor(function=None):
+    @wraps(function)
+    def wrapper(*args,**kwargs):
+        _=function(*args,**kwargs)
+        print(("Ip Address : {}".format(request.remote_user)))
+        print("Cookies : {} ".format(request.cookies))
+        print("User Agent : {}".format(request.user_agent))
+        print("Remote User : {} ".format(request.remote_user))
+        return _
+    return wrapper
+
+
+
+
+class HelloWorld(Resource):
+
+    @monitor
+    def get(self):
+
+
+        return {'hello':'world'}
+
+api.add_resource(HelloWorld,'/')
+
+if __name__=="__main__":
+    app.run(debug=True)
