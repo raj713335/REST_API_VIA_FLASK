7cbdf44c79add887b35c58087e4d88b2798e15ea
ARNAB DAS <raj713335@gmail.com>
2020-10-25 05:35:59 +0530

    Initial Commit

--- 15 You Just wrote Flask Rest API How to write a Unit Test for Rest API in Python.py
+++ 15 You Just wrote Flask Rest API How to write a Unit Test for Rest API in Python.py
@@ -13,6 +13,19 @@ class FlaskTest(unittest.TestCase):
         satuscode=response.status_code
         self.assertEqual(satuscode,200)
 
+    # check if the content return is application?json
+    def test_index_content(self):
+        tester=app.text_client(self)
+        response=tester.get("/fo")
+        self.assertEqual(response.content_type,"application/json")
+
+    # check for DAta returned
+    def test_index_data(self):
+        tester=app.test_client(self)
+        response=tester.get("/fo")
+        self.assertTrue(b'Message' in response.data)
+
+
 if __name__=="__main__":
     unittest.main()
 
